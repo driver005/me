22162b75de5cb80aeb8675da7577d3230abc8af1
driver005 <github@dark-lord.de>
2026-02-21 19:51:53 +0100

    fix(adaper): else deployment is not working!!

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { threlteStudio } from '@threlte/studio/vite'
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { threlteStudio } from "@threlte/studio/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter({
+      fallback: "404.html", // or 'index.html' if you want SPA-style routing
+    }),
   },
   plugins: [threlteStudio()],
   compilerOptions: {
     // runes: true,
-  }
+  },
 };
 
 export default config;
+
