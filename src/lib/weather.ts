2e3c12b41781d93ee0ea57d3c7f7ce0f56d8e8ae
driver005 <github@dark-lord.de>
2026-02-22 16:29:03 +0100

    feat(music): remove spotify security flaw!!

--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -1,15 +1,23 @@
 export async function check_weather() {
   try {
-    const response = await fetch("/api/weather");
+    const lat = 48.521637;
+    const lon = 9.057645;
+    const url =
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=precipitation,weather_code`;
 
-    // Check if the response is actually okay (200-299 status)
+    const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const weather = await response.json();
+    const data = await response.json();
 
-    return weather.isRaining;
+    // Mapping the weather data into a clean object
+    const isRaining = data.current.precipitation > 0 ||
+      data.current.weather_code >= 51;
+
+    return isRaining;
   } catch (e) {
     console.error("Failed to fetch weather route", e);
     return null;
--- src/routes/api/recently-played/+server.ts
+++ src/routes/api/recently-played/+server.ts
@@ -1,5 +1,5 @@
-import { json } from '@sveltejs/kit';
-import { getRecentlyPlayed } from '$lib/spotify';
+import { json } from "@sveltejs/kit";
+import { getRecentlyPlayed } from "$lib/spotify";
 
 export async function GET() {
   const response = await getRecentlyPlayed();
@@ -7,10 +7,10 @@ export async function GET() {
 
   const tracks = items.map((item: any) => ({
     title: item.track.name,
-    artist: item.track.artists.map((_artist: any) => _artist.name).join(', '),
+    artist: item.track.artists.map((_artist: any) => _artist.name).join(", "),
     albumImageUrl: item.track.album.images[0].url,
     songUrl: item.track.external_urls.spotify,
-    playedAt: item.played_at
+    playedAt: item.played_at,
   }));
 
   return json(tracks);
